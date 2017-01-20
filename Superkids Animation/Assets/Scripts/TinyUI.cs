3c1f3dd52496120cf729de3fee40df1711f2c17a
kertaskids <kertaskids@gmail.com>
2017-01-20 14:55:54 +0800

    Superkids Animation
    Simple unity 2D animation

---
+++ Superkids Animation/Assets/Scripts/TinyUI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TinyUI : MonoBehaviour {
+
+	public Animator animator;
+
+	void Start () {
+		BounceIn ();
+	}
+
+	void Update (){
+		if (transform.localScale.x <= 0f) {
+			GoToScreen("SuperKids Animation");
+		}
+	}
+
+	private void BounceIn(){
+		animator.SetBool ("BounceIn", true);
+	}
+
+	public void BounceOut(){
+		animator.SetBool ("BounceIn", false);
+		animator.SetBool ("BounceOut", true);
+	}
+
+	public void GoToScreen (string screen){
+		Application.LoadLevel (screen);
+	}
+}
---
+++ Superkids Animation/Assets/readme TinyUI.txt
@@ -0,0 +1,6 @@
+Readme :
+0) copy file Button.animation, BounceIn.anim, BounceOut.anim
+1) Buat object button
+2) masukan animator ke Button
+3) masukan script TinyUI ke Button & populate animator ke script TinyUI
+4) buat method pada script TinyUi untuk berpindah screen
